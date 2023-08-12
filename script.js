8508b5690891e80f98d1aa6d1e05809fe11aeadc
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-12 23:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const team = players;
 const team1 = ["John", "Bob", "Alice", "Poppy"];
 
 
-const cap1 = Object.assign(person);
+const cap1 = Object.assign({}, person);
 
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
